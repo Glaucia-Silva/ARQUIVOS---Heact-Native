80786d368743ddc54b36127851f7e9d0a272763c
Glaucia-Silva <glauciadev@gmail.com>
2023-08-11 15:30:30 -0300

    initial commit

---
+++ project-name/src/App.js
@@ -0,0 +1,19 @@
+import './App.css'
+import SayMyName from './components/SayMyName'
+import Pessoa from './components/Pessoa'
+
+function App () {
+  const nome= 'Maria'
+  
+  return(
+    <div className="App">
+      <SayMyName nome="Glaucia" />
+      <SayMyName nome="João" />
+      <SayMyName nome={nome} />
+      <Pessoa nome="Rodrigo" idade="28" profissao="Programador" foto="https://via.placeholder.com/150"
+      />
+    </div>
+  )
+}
+
+export default App
---
+++ project-name/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+        <div>
+            <p>Este é meu componente com frase!</p>
+        </div>
+    )
+}
+
+export default Frase
